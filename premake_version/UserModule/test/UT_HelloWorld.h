2040e9553aa5d8736d3610f5f54321257de1197d
Chen Guodong <sinojelly@163.com>
2010-10-17 00:07:58 +0800

    use new version of mockcpp, can run test with global mocker or object mocker without any mem leak failure reported. and even reset is not needed.

--- premake_version/UserModule/test/UT_HelloWorld.h
+++ premake_version/UserModule/test/UT_HelloWorld.h
@@ -1,11 +1,20 @@
 #include <testngpp/testngpp.hpp>
 #include <mockcpp/mokc.h>
 #include <mem_checker/interface_4user.h>
+#include <mockcpp/mockcpp.hpp>
 
 #include <UserModule/HelloWorld.h>
 
+struct Interface 
+{
+    virtual int func() = 0;
+    virtual ~Interface(){}
+};
+
 FIXTURE(HelloWorld, base test)
 {
+    MockObject<Interface> mocker;
+
 	TEST(1 + 2 => 3)
 	{
 	    ASSERT_EQ(3, add(1, 2));
@@ -17,18 +26,26 @@ FIXTURE(HelloWorld, base test)
 		delete [] p;
 	}
 	
-	TEST(mockcpp test)
+	TEST(mockcpp global mocker test)
 	{
-        STOP_MEM_CHECKER(); // TODO: maybe mockcpp leaked one object
-
 	    MOCKER(add)
 		  .stubs()
 		  .will(returnValue(200));
 		ASSERT_EQ(200, add(1, 2));
 
         GlobalMockObject::verify();
-        GlobalMockObject::reset();
+        //GlobalMockObject::reset();
 	}
+
+    TEST(mockcpp mock object test)
+    {        
+        MOCK_METHOD(mocker, func)
+            .expects(once())
+            .will(returnValue(3));
+        ASSERT_EQ(3, mocker->func());
+        mocker.verify();
+        //mocker.reset();
+    }
 
     // @test(id = parent1)
     TEST(±ª“¿¿µ1)
