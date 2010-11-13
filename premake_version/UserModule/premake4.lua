1b44f017a8a4cc7c799cfc296cd5c4643a3cfd08
Chen Guodong <sinojelly@163.com>
2010-11-14 01:38:08 +0800

    support add .cpp/.h/test files for class or feature. test ok.

--- premake_version/UserModule/premake4.lua
+++ premake_version/UserModule/premake4.lua
@@ -5,6 +5,8 @@
 --             premake4 gmake     and then : make
 --             premake4 gmake     and then : make config=debug
 --
+-- you can also add files for class or feature, use "premake4 --usage addfile" to show more.
+--
 
 --
 -- define user's parameters, all the paths are relative to premake4.lua dir.
@@ -12,22 +14,26 @@
 
 -- change the module name by yourself
 module_name     = "UserModule"
+
+-- define the tools dir
 tools_dir       = "../tools"
 
 -- define the user's include dir and librarys. (eg: {"something", "otherthing"})
+-- action addfile will add .h file to the first include dir.
 include_dirs    = {"include"}
 librarys        = {}
 librarys_dirs   = {}
 
 -- define .cpp/test files search path (it will search recursively)
+-- action addfile will add .cpp/test file to the first src/test dir.
 src_files_dirs  = {"src"}
 test_files_dirs = {"test"}
 
 cwd = os.getcwd()
 
-dofile(tools_dir.."/scripts/addclass.lua")
+dofile(tools_dir.."/scripts/addfile.lua")
 
-if _ACTION == "addclass" or _ACTION == "addtest" then return end
+if _ACTION == "addfile" then return end
 
 dofile(tools_dir.."/scripts/testngpp.lua")
 
--- premake_version/tools/scripts/addfile.lua
+++ premake_version/tools/scripts/addfile.lua
@@ -1,5 +1,6 @@
 --
---  Add class's .h/.cpp/test files, or add test file only. 
+--  Add .h/.cpp/test files for class, or add test file for feature. 
+--  If one file exists, will not overwrite it, but still add others.
 --  Author : chenguodong <sinojelly@gmail.com>
 --  2010.11.13
 --    
@@ -15,64 +16,97 @@ newoption {
 }
 
 newoption {
-   trigger     = "help",
+   trigger     = "usage",
    description = "Show usage of addclass."
+}
+
+newoption {
+   trigger     = "class",
+   description = "Name of the class that you want to add."
+}
+
+newoption {
+   trigger     = "feature",
+   description = "Name of the feature that you want to add test."
 }
 
 ---------------------------------
 
-function help()
-	if not _OPTIONS["help"] then return false end
+function usage()
+	if not _OPTIONS["usage"] then return false end
 	
 	print("Usage: ")
-	print("    add class's h file          :   premake4 addclass ClassName")
-	print("    add class's h/cpp file      :   premake4 --cpp addclass ClassName")
-	print("    add class's h/cpp/test file :   premake4 --test addclass ClassName")
-	print("    add test file               :   premake4 addtest FeatureName")
+	print("    add .h file for class                   :   premake4 --class=ClassName addfile")
+	print("    add .h and .cpp file for class          :   premake4 --class=ClassName --cpp addfile")
+	print("    add .h and .cpp and test file for class :   premake4 --class=ClassName --test addfile")
+	print("    add test file for feature               :   premake4 --feature=FeatureName addfile")
 	
 	return true
 end
 
+function is_class()
+	if not _OPTIONS["class"] then return false end
+	return true
+end
+
+function is_feature()
+	if not _OPTIONS["feature"] then return false end
+	return true
+end
+
+function class_name()
+	if is_class() then return _OPTIONS["class"] end
+end
+
+function feature_name()
+	if is_feature() then return _OPTIONS["feature"] end
+end
+
 ---------------------------------
 
+function get_template_dir()
+	return tools_dir.."/scripts/"
+end
+
 function get_h_template_path()
-	return "Template.h"
+	return get_template_dir().."Template.h"
 end
 
 function get_cpp_template_path()
-	return "Template.cpp"
+	return get_template_dir().."Template.cpp"
 end
 
 function get_test_template_path()
-	return "TemplateTest.h"
+	return get_template_dir().."TemplateTest.h"
 end
 
 ---------------------------------
+-- Note: current dir is UserModule!!!
 
-local path_offset = path.getrelative(os.getcwd(), cwd).."/"
+local path_offset = "" -- path.getrelative(os.getcwd(), cwd).."/"
 
 function get_h_dir()
 	if not include_dirs[1] then
-		print("ERROR: no include_dirs specified.")
+		error("ERROR: no include_dirs specified.")
 		return ""
 	end
-	return path_offset..include_dirs[1]
+	return path_offset..include_dirs[1].."/"..module_name.."/"
 end
 
 function get_cpp_dir()
 	if not src_files_dirs[1] then
-		print("ERROR: no src_files_dirs specified.")
+		error("ERROR: no src_files_dirs specified.")
 		return ""
 	end
-	return path_offset..src_files_dirs[1]
+	return path_offset..src_files_dirs[1].."/"
 end
 
 function get_test_dir()
 	if not test_files_dirs[1] then
-		print("ERROR: no test_files_dirs specified.")
+		error("ERROR: no test_files_dirs specified.")
 		return ""
 	end
-	return path_offset..test_files_dirs[1]
+	return path_offset..test_files_dirs[1].."/"
 end
 
 ---------------------------------
@@ -92,15 +126,15 @@ end
 ---------------------------------
 
 function get_h_file_path(class)
-	return get_h_dir().."/"..get_h_file_name(class)
+	return get_h_dir()..get_h_file_name(class)
 end
 
 function get_cpp_file_path(class)
-	return get_cpp_dir().."/"..get_cpp_file_name(class)
+	return get_cpp_dir()..get_cpp_file_name(class)
 end
 
 function get_test_file_path(class)
-	return get_test_dir().."/"..get_test_file_name(class)
+	return get_test_dir()..get_test_file_name(class)
 end
 
 ---------------------------------
@@ -125,14 +159,15 @@ function need_h_file(class)
 end
 
 function need_cpp_file(class)
-	if not _OPTIONS["test"] and not _OPTIONS["cpp"] then return false end
 	if exist_cpp_file(class) then return false end
+	if not _OPTIONS["test"] and not _OPTIONS["cpp"] then return false end
 	return true
 end
 
 function need_test_file(class)
-	if not _OPTIONS["test"] then return false end
 	if exist_test_file(class) then return false end
+	if not not _OPTIONS["feature"] then return true end -- Note: must use not not
+	if not _OPTIONS["test"] then return false end
 	return true
 end
 
@@ -150,13 +185,14 @@ function original(name)
 	return name
 end
 
+-- Note: not this: sed -i -e 's/abc/ABC/g' file.h
 function replace_module(file, translate)
-	local cmdline = "sed -i -e 's/"..translate(get_old_module_name()).."/"..translate(module_name).."/g' "..file
+	local cmdline = "sed -i -e s/"..translate(get_old_module_name()).."/"..translate(module_name).."/g "..file
 	os.execute(cmdline)
 end
 
 function replace_class(file, class, translate)
-	local cmdline = "sed -i -e 's/"..translate(get_old_class_name()).."/"..translate(class).."/g' "..file
+	local cmdline = "sed -i -e s/"..translate(get_old_class_name()).."/"..translate(class).."/g "..file
 	os.execute(cmdline)
 end
 
@@ -166,6 +202,10 @@ function replace(file, class)
 	
 	replace_class(file, class, original)
 	replace_class(file, class, string.upper)
+	
+	local dir = path.getdirectory(file)
+	os.chdir(dir) -- must chdir first
+	os.execute("del sed*") -- Warning: del temp file, may del some usefull file start with sed
 end
 
 ---------------------------------
@@ -186,14 +226,17 @@ end
 
 function copy_h_file(class)
 	os.copyfile(get_h_template_path(), get_h_file_path(class))
+	print("add "..get_h_file_path(class))
 end
 
 function copy_cpp_file(class)
 	os.copyfile(get_cpp_template_path(), get_cpp_file_path(class))
+	print("add "..get_cpp_file_path(class))
 end
 
 function copy_test_file(class)
 	os.copyfile(get_test_template_path(), get_test_file_path(class))
+	print("add "..get_test_file_path(class))
 end
 
 ---------------------------------
@@ -232,29 +275,24 @@ end
 
 ---------------------------------
 
-function get_argument()
-	if not arg then return nil end
-	return arg[1]
-end
-
----------------------------------
-
 newaction {
-   trigger     = "addclass",
-   description = "Add class files",
+   trigger     = "addfile",
+   description = "Add .h/.cpp/test files for class/feature(use --usage to show help)",
    execute = function ()
-	  if help() then return end	  
-	  add_h_file(get_argument())
-	  add_cpp_file(get_argument())
-	  add_test_file(get_argument())
-   end
-}
+	  if usage() then return end
 
-newaction {
-   trigger     = "addtest",
-   description = "Add test file",
-   execute = function ()
-      if help() then return end
-	  add_test_file(get_argument())
+	  if is_class() then
+	     add_h_file(class_name())
+	     add_cpp_file(class_name())
+	     add_test_file(class_name())
+		 return
+	  end
+	  
+	  if is_feature() then
+	     add_test_file(feature_name())
+		 return
+	  end
+	  
+	  error("ERROR: no class name or feature name specified!")
    end
 }
