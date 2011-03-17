4e7aa7c1f9eff2b94fcda031e64c6f77eaf946e4
Chen Guodong <sinojelly@163.com>
2011-03-18 00:33:57 +0800

    add ExportLite.ps1

---
+++ ExportLite.ps1
@@ -0,0 +1,16 @@
+# export mockcpp without boost
+
+$env:path=$env:path+";D:\Tools\CMD\SubversionClient"
+
+$dest_dir="D:\tools_release\cpp-ut-project_exported"
+
+function export_subdir($subdir) 
+{
+	if (!(test-path $dest_dir\$subdir)) { mkdir $dest_dir\$subdir }
+	xcopy $subdir $dest_dir\$subdir\ /S	
+}
+
+export_subdir tools\scripts
+export_subdir UserModule
+
+xcopy readme.txt $dest_dir\
